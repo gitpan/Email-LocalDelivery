74b9e23fd271ce739076bbbbc4c88ce733d50a09
Simon Cozens <simon@cpan.org>
2003-03-17 15:43:05 +0000

    Import of SIMON/Email-LocalDelivery-0.01 from CPAN.
    gitpan-cpan-distribution: Email-LocalDelivery
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         SIMON/Email-LocalDelivery-0.01.tar.gz
    gitpan-cpan-author:       SIMON
    gitpan-cpan-maturity:     released

---
+++ LocalDelivery.pm
@@ -0,0 +1,77 @@
+package Email::LocalDelivery;
+
+require 5.005_62;
+use strict;
+use warnings;
+use Carp;
+our $VERSION = '0.01';
+
+=head1 NAME
+
+Email::LocalDelivery - Deliver a piece of email - simply
+
+=head1 SYNOPSIS
+
+  use Email::LocalDelivery;
+  my $delivery_agent = Email::LocalDelivery->deliver($mail, @boxes);
+
+=head1 DESCRIPTION
+
+This is the second module produced by the "Perl Email Project", a
+reaction against the complexity and increasing bugginess of the
+C<Mail::*> modules. It delivers an email to a list of mailboxes.
+
+=head1 METHODS
+
+=head2 deliver
+
+This takes an email, as a plain string, and a list of mailboxes to
+deliver that mail to. It returns the list of boxes actually written to.
+If no boxes are given, it assumes the standard Unix mailbox. (Either
+C<$ENV{MAIL}>, F</var/spool/mail/you>, F</var/mail/you>, or
+F<~you/Maildir/>)
+
+=cut
+
+sub deliver {
+    my ($class, $mail, @boxes) = @_;
+    croak "Mail argument to deliver should just be a plain string"
+        if ref $mail;
+    if (!@boxes) {
+        my $default_unixbox = ( grep { -d $_ } qw(/var/spool/mail/ /var/mail/) )[0] . getpwuid($>);
+        my $default_maildir = ((getpwuid($>))[7])."/Maildir/";
+
+        @boxes = $ENV{MAIL}
+            || (-e $default_unixbox && $default_unixbox)
+            || (-d $default_maildir."cur" && $default_maildir);
+
+    }
+    my %to_deliver;
+    push @{$to_deliver{type_of($_)}}, $_ for @boxes;
+    for my $method (keys %to_deliver) {
+        eval "require Email::LocalDelivery::$method";
+        croak "Couldn't load a module to handle $method mailboxes" if $@;
+        "Email::LocalDelivery::$method"->deliver($mail,
+                                                @{$to_deliver{$method}});
+    }
+}
+
+sub type_of {
+    local $_ = shift;
+    return "Maildir" if m{/$};
+    return "MH"      if m{/\.$};
+    return "Maildir" if -d;
+    return "Mbox";
+}
+
+1;
+__END__
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2003 by Simon Cozens
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself. 
+
+=cut
---
+++ LocalDelivery/Mbox.pm
@@ -0,0 +1,70 @@
+package Email::LocalDelivery::Mbox;
+use File::Path;
+use File::Basename;
+use Email::Simple;
+use Fcntl ':flock';
+
+our $VERSION = "1.0";
+
+sub deliver {
+    my ($class, $mail, @files) = @_;
+    my @rv;
+    for my $file (@files) {
+        my $dir = dirname($file);
+        next if ! -d $dir and not mkpath($dir);
+
+        open my $fh, ">> $file"               || next;
+        $class->getlock($fh)                  || next;
+        seek $fh, 0, 2;
+        print $fh $class->_from_line(\$mail); # Avoid passing $mail where poss.
+        print $fh $mail;
+        print $fh "\n";
+        $class->unlock($fh)                   || next;
+        close $fh                             || next;
+        push @rv, $file
+    }
+    return @rv;
+}
+
+sub _from_line {
+    my ($class, $mail_r) = @_;
+
+    # The trivial way
+    return if $$mail_r =~ /^From\s/;
+
+    # The qmail way.
+    return $ENV{UFLINE}.$ENV{RPLINE}.$ENV{DTLINE} if exists $ENV{UFLINE};
+    
+    # The boring way.
+    return _from_line_boring(Email::Simple->new($$mail_r));
+}
+
+sub _from_line_boring {
+    my $mail = shift;
+    my $from = $mail->header("Return-path") ||
+               $mail->header("Sender")      ||
+               $mail->header("Reply-To")    ||
+               $mail->header("From")        ||
+               'root@localhost';
+    $from = $1 if $from =~ /<(.*?)>/; # comment <email@address> -> email@address
+    $from =~ s/\s*\(.*\)\s*//;        # email@address (comment) -> email@address
+    $from =~ s/\s+//g; # if any whitespace remains, get rid of it.
+
+    my $fromtime = localtime;
+    $fromtime =~ s/(:\d\d) \S+ (\d{4})$/$1 $2/; # strip timezone.
+    return "From $from  $fromtime\n";
+}
+
+sub getlock {
+    my ($class, $fh) = @_;
+    for (1..10) {
+        return 1 if flock ($fh, LOCK_EX | LOCK_NB);
+        sleep $_;
+    }
+    return 0 ;
+}
+
+sub unlock {
+    my ($class,$fh) = @_;
+    flock ($fh, LOCK_UN);
+}
